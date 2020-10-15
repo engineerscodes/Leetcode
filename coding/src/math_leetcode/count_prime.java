71b73db8204f778a27571cbf2ab584465fd5914d
engineerscodes <practicalengineers05@gmail.com>
2020-10-15 13:54:07 +0530

    Initial Commit

---
+++ coding/src/math_leetcode/count_prime.java
@@ -0,0 +1,38 @@
+package math_leetcode;
+
+public class count_prime
+{
+
+	public count_prime() 
+	{
+		
+	}
+
+	public static void main(String[] args) 
+	{ /*int s=1;
+	  int flag=0; 
+	for(int i=1;i<500000;i=i+2)
+	{
+		for(int j=2;j<i;j++)
+		{
+			if(i%j==0)
+			{  flag=0;
+					break;
+					
+			}
+			else
+				flag=1;
+				
+		}
+		if(flag==1)
+			{
+			 s++;
+			
+			}
+	}
+	System.out.println(" count "+(s));
+	*/ //Time limit exeeeded
+		
+	}
+
+}
